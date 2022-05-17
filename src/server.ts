4490455fa68f8e2f03dbef31cfafcb93005c49ad
Nadav Nassi <nadav.nassi@gmail.com>
2022-05-17 15:09:53 +0300

    minor

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors(corsOptions))
 }
 
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
 app.use('/api/movie', movieRoutes)
 
 app.get('/**', (req: Request, res: Response) => {
